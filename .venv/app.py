38788918c75f1bf3eada2f4fe72d9a3deb291eca
Andrew Hom <andrewhom77@gmail.com>
2024-12-06 03:58:15 -0800

    First commit
    Basic setup done?

---
+++ .venv/app.py
@@ -0,0 +1,15 @@
+from flask import Flask
+from flask_sqlalchemy import SQLAlchemy
+from sqlalchemy.orm import DeclarativeBase
+
+class Base(DeclarativeBase):
+    pass
+
+db = SQLAlchemy(model_class=Base)
+
+app = Flask(__name__)
+app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///final.sqlite"
+db.init_app(app)
+
+with app.app_context():
+    db.create_all()
