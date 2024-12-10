2e5f49a51cacf4fbce4f0a791624c4d960e96e2d
Andrew Hom <andrewhom77@gmail.com>
2024-12-10 09:36:09 -0800

    Added render_template, default route
    login not working yet

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask
+from flask import Flask, render_template
 from flask_sqlalchemy import SQLAlchemy
 
 app = Flask(__name__)
@@ -7,3 +7,7 @@ db = SQLAlchemy(app)
 
 with app.app_context():
     db.create_all()
+
+@app.route('/')
+def default_page():
+    return render_template('forum.html')
