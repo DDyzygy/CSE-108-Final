ac3490da0c2a77bc9597bcd8dc9c3cbf05510b5b
Cluchetta2003 <luchetta2015@outlook.com>
2024-12-10 22:31:01 -0800

    Added sign up page and started password hashing
    Created a new page solely for adding new users. When the login credentials are entered, they're to be comited to the main database and the user is taken back to the login page. I also started work on password hashing and would like you guys to take a look and see if you can add to it and test it to get it running.

--- app.py
+++ app.py
@@ -1,9 +1,21 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request
 from flask_sqlalchemy import SQLAlchemy
 
+from werkzeug.security import generate_password_hash, check_password_hash 
+
 app = Flask(__name__)
+app.config['SECRET_KEY'] = 'our_key'
 app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///final.sqlite"
 db = SQLAlchemy(app)
+
+class User(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    username = db.Column(db.String(80), unique=True, nullable=False)
+    password_hash = db.Column(db.String(200), nullable=False)
+
+# app = Flask(__name__)
+# app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///final.sqlite"
+# db = SQLAlchemy(app)
 
 with app.app_context():
     db.create_all()
@@ -11,3 +23,19 @@ with app.app_context():
 @app.route('/')
 def default_page():
     return render_template('forum.html')
+
+@app.route('/signup', methods=['POST'])
+def signup_page():
+    username = request.form.get('username')
+    password = request.form.get('password')
+
+    hashed_password = generate_password_hash(password)
+
+    new_user = User(username=username, password_hash = hashed_password)
+    db.session.add(new_user)
+    db.session.commit()
+
+
+
+if __name__ == "__main__":
+    app.run(debug=True)
